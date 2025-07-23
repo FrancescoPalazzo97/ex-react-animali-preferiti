a4087f61605ca307ce2129c42d2cbd99b8941884
FrancescoPalazzo97 <francescopalazzo97@gmail.com>
2025-07-23 20:28:16 +0200

    UPD - Create separate components

---
+++ components/Modal.jsx
@@ -0,0 +1,40 @@
+const { createPortal } = ReactDOM;
+
+function Modal({
+    title,
+    content,
+    show = false,
+    search,
+    setSearch,
+    onClose = () => { },
+    onConfirm = () => { },
+    success = false,
+    error = false
+}) {
+    return show && createPortal(
+        <div className="modal-container">
+            <div className="modal">
+                <h2>{title}</h2>
+                <input
+                    type="text"
+                    placeholder="Cerca..."
+                    value={search}
+                    onChange={e => setSearch(e.target.value)}
+                />
+                <button onClick={onConfirm}>Conferma</button>
+                <button onClick={onClose}>Annulla</button>
+                {success &&
+                    <div>
+                        <span className='success'>Elemento aggiunto alla lista</span>
+                    </div>
+                }
+                {error &&
+                    <div>
+                        <span className='error'>Elemento non trovato</span>
+                    </div>
+                }
+            </div>
+        </div>,
+        document.body
+    )
+}
---
+++ js/script.js
@@ -0,0 +1,106 @@
+const { createRoot, createPortal } = ReactDOM;
+const { useState } = React;
+const API = 'http://localhost:3333/animals?search=';
+
+const Animals = () => {
+
+    const animalsChoices = [
+        'Gatto',
+        'Cane',
+        'Scimmia',
+        'Elefante',
+        'Leone',
+        'Tigre',
+        'Orso',
+        'Lupo',
+        'Volpe'
+    ];
+
+    const [animalsArray, setAnimalsArray] = useState([]);
+    const [show, setShow] = useState(false);
+    const [search, setSearch] = useState(``);
+    const [success, setSuccess] = useState(false);
+    const [error, setError] = useState(false)
+
+    function addAnimal() {
+        const randomIndex = Math.floor(Math.random() * animalsChoices.length);
+        setAnimalsArray([...animalsArray, animalsChoices[randomIndex]]);
+    }
+
+    function onClose() {
+        setShow(false);
+        setSuccess(false);
+        setError(false);
+        setSearch('');
+    }
+
+    function onOpen() {
+        setShow(true);
+    }
+
+    async function fetchJson(url) {
+        try {
+            const res = await fetch(url);
+            return await res.json();
+        } catch (e) {
+            console.error(`Errore durante la ricerca dell'animale:\n`, e)
+        }
+    }
+
+    async function onConfirm() {
+        setSuccess(false);
+        setError(false);
+
+        let message = 'Elemento non presente';
+
+        const term = await fetchJson(`${API}${search}`);
+
+        if (!term) message = `Errore durante la ricerca dell'animale`
+
+        if (!term || term.length === 0) {
+            console.error(message);
+            setError(true)
+            setSearch('');
+        } else {
+            animalsArray.push(term[0]);
+            setSuccess(true);
+            setSearch('');
+            setTimeout(() => onClose(), 2000)
+        }
+    }
+
+    return (
+        <>
+            <button onClick={onOpen}>
+                Aggiungi un animale!!!
+            </button>
+            <Modal
+                title={`Cerca l'animale da aggiungere`}
+                show={show}
+                onClose={onClose}
+                onConfirm={onConfirm}
+                search={search}
+                setSearch={setSearch}
+                success={success}
+                error={error}
+            />
+            <details>
+                <summary>
+                    <h2>Animali</h2>
+                </summary>
+                <ul>
+                    {animalsArray.map((animal, i) => (
+                        <li key={i}>
+                            <Card animal={animal} />
+                        </li>
+                    )
+                    )}
+                </ul>
+            </details>
+        </>
+    )
+}
+
+const listaAnimali = document.querySelector('.lista-animali');
+const root = createRoot(listaAnimali);
+root.render(<Animals />)
